e07f5f5e691ac045cd9d426f44edaaf9b3d07057
Rifki Maulana <rifkkimaulana@gmail.com>
2023-04-24 01:17:59 +0700

    Menambahkan fitur tambah data

--- kuliah/pertemuan5/functions.php
+++ kuliah/pertemuan5/functions.php
@@ -6,12 +6,32 @@ function koneksi()
 function query($query)
 {
     $conn = koneksi();
-    
+
     $result = mysqli_query($conn, $query);
 
     while ($row = mysqli_fetch_assoc($result)) {
     $rows[] = $row;
     }
     return $rows;
+}
+function tambah($data) {
+    //var_dump($data);
+    $conn = koneksi();
+    $nama = htmlspecialchars($data['nama']);
+    $nim = htmlspecialchars($data['nim']);
+    $email = htmlspecialchars($data['email']);
+    $jurusan = htmlspecialchars($data['jurusan']);
+    $gambar = htmlspecialchars($data['gambar']);
+    $query = "INSERT INTO
+                mahasiswa
+            VALUES
+            (null, '$nama', '$nim', '$email', '$jurusan', '$gambar')
+            ";
+    mysqli_query($conn, $query);
+
+    echo mysqli_error($conn);
+
+    return mysqli_affected_rows($conn);
+
 }
  ?>
